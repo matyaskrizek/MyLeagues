3e151491772b140499287e8cb4af94556ee4e9ad
Matyas Krizek <mkrizek@starsinc.com>
2019-11-08 13:49:41 -0800

    sing up button and background

--- MyLeague/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ MyLeague/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -82,11 +82,11 @@ public final class R {
         public static final int parent = 0x7f07005c;
         public static final int percent = 0x7f07005e;
         public static final int right = 0x7f070062;
-        public static final int spread = 0x7f07007c;
-        public static final int spread_inside = 0x7f07007d;
-        public static final int start = 0x7f070082;
-        public static final int top = 0x7f070091;
-        public static final int wrap = 0x7f070097;
+        public static final int spread = 0x7f07007d;
+        public static final int spread_inside = 0x7f07007e;
+        public static final int start = 0x7f070083;
+        public static final int top = 0x7f070092;
+        public static final int wrap = 0x7f070098;
     }
     public static final class styleable {
         private styleable() {}
