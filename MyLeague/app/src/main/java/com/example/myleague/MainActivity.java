d7dde0595ca093beccfdbd7b0687c2a609e24f75
Robert Sapien <robertsapien@wrls-109-58.sonoma.edu>
2019-11-08 13:57:31 -0800

    Added Home.java and activity_home.xml. Working on moving to a new page from login page.

--- MyLeague/app/src/main/java/com/example/myleague/MainActivity.java
+++ MyLeague/app/src/main/java/com/example/myleague/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.myleague;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button login_button;
+    private TextView userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userName = findViewById(R.id.search_edit_text_username);
+        login_button = findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = userName.getText().toString();
+                Intent intent = new Intent(MainActivity.this, Home.class);
+
+                intent.putExtra(Home.NAME_KEY, name);
+                startActivity(intent);
+            }
+        });
     }
 }
